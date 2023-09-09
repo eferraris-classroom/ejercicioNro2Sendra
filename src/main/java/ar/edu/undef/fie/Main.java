93c8fbd9025886f8b6182abfe2607aa0fdb387fa
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-09 19:47:18 -0300

    Cambio en la visualizacion

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -3,13 +3,17 @@ package ar.edu.undef.fie;
 public class Main {
     public static void main(String[] args)
     {
-    System.out.print("Ingreso los siguientes argumentos: ");
-        for (String argumento : args)
-        {
-            System.out.print(argumento + " ") ;
+
+    if (args.length == 0)
+    {
+        System.out.println("No proporciono elementos");
+    } else {
+
+        System.out.print("Ingreso los siguientes argumentos: ");
+        for (String argumento : args) {
+            System.out.print(argumento + " ");
         }
-
-
+    }
 
 
 
