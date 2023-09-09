3d8088740116d83100f025f7f6f15630d6586820
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-09 19:38:01 -0300

    Agregar visualizacion de argumentos cargados

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,7 +1,17 @@
 package ar.edu.undef.fie;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+    System.out.print("Ingreso los siguientes argumentos: ");
+        for (String argumento : args)
+        {
+            System.out.print(argumento + " ") ;
+        }
+
+
+
+
+
     }
 }
